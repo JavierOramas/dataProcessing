362500fbb2ee58638d52e9024d3b7aa09b0b9fd8
JavierOramas <javiale2000@gmail.com>
2021-07-27 15:27:18 -0400

    -

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 from tools import process
 
-# if st.button('Comenzar procesamiento'):
-#     code = process()
-#     if code == 'error':
-#         st.danger('hubo errores')
-#     elif code == 'notifications':
-#         st.info('se corrigieron automaticamente algunos errores')
-#     else:
-#         st.success('procesado correctamente')
-#         # 
-process()
+if st.button('Comenzar procesamiento'):
+    code = process()
+    if code == 'error':
+        st.danger('hubo errores')
+    elif code == 'notifications':
+        st.info('se corrigieron automaticamente algunos errores')
+    else:
+        st.success('procesado correctamente')
+        # 
+# process()
